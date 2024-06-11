0600b411d1e1db2506b1ae80908f9d52fc37d859
bajjurirohan <bajjurirohan@gmail.com>
2024-06-11 15:10:06 -0400

    final changes

--- coding_challenge/movies/migrations/0004_populate_reviews.py
+++ coding_challenge/movies/migrations/0004_populate_reviews.py
@@ -11,12 +11,19 @@ def populate_reviews(apps, schema_editor):
         {"movie_title": "Forrest Gump", "name": "Reviewer 2", "rating": 5},
         {"movie_title": "Toy Story", "name": "Reviewer 3", "rating": 4},
         {"movie_title": "Toy Story", "name": "Reviewer 4", "rating": 5},
+        {"movie_title": "Captain Phillips", "name": "Reviewer 5", "rating": 3},
+        {"movie_title": "Captain Phillips", "name": "Reviewer 6", "rating": 2},
+        {"movie_title": "Catch Me If You Can", "name": "Reviewer 7", "rating": 2},
+        {"movie_title": "Catch Me If You Can", "name": "Reviewer 8", "rating": 4},
+        {"movie_title": "Catch Me If You Can", "name": "Reviewer 9", "rating": 2},
+        {"movie_title": "Bridge of Spies", "name": "Reviewer 10", "rating": 4},
+        {"movie_title": "Bridge of Spies", "name": "Reviewer 11", "rating": 4},
     ]
 
-    # for review_data in reviews_data:
-    #     movie_title = review_data.pop("movie_title")
-    #     movie = Movie.objects.using(db_alias).get(title=movie_title)
-    #     Review.objects.using(db_alias).create(movie=movie, **review_data)
+    for review_data in reviews_data:
+        movie_title = review_data.pop("movie_title")
+        movie = Movie.objects.using(db_alias).get(title=movie_title)
+        Review.objects.using(db_alias).create(movie=movie, **review_data)
 
 def empty_reviews(apps, schema_editor):
     Review = apps.get_model("movies", "Review")
--- coding_challenge/movies/views/movie_list.py
+++ coding_challenge/movies/views/movie_list.py
@@ -5,5 +5,20 @@ from movies.serializers import MovieSerializer
 
 
 class MovieListView(ListCreateAPIView):
-    queryset = Movie.objects.order_by("id")
     serializer_class = MovieSerializer
+
+    def get_queryset(self):
+        queryset = Movie.objects.order_by("id")
+
+        runtime = self.request.query_params.get('runtime')
+        filter_type = self.request.query_params.get('filter')  # 'longer' or 'shorter'
+
+
+        if runtime and filter_type:
+            runtime = int(runtime)
+            if filter_type == 'longer':
+                queryset = queryset.filter(runtime__gt=runtime)
+            elif filter_type == 'shorter':
+                queryset = queryset.filter(runtime__lt=runtime)
+
+        return queryset
